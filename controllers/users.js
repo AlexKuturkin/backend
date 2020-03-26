e13872b9a6a81637aa074e3c99f2ec24f92a0af2
Alex Kuturkin <alexkuturkin@mail.ru>
2020-03-26 18:00:53 +0300

    13 sprint 2

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,34 @@
 const user = require("../models/user");
 
 module.exports.getUsers = (req, res) => {
-  user.find({})
+  user
+    .find({})
     .then((users) => res.send({ data: users }))
-    .catch((error) => res.status(500).send({ message: "Ошибка при выводе всех пользователей", err: error }));
+    .catch((error) => res
+      .status(500)
+      .send({ message: "Ошибка при выводе всех пользователей", err: error }));
 };
 
 module.exports.getUserById = (req, res) => {
-  user.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(500).send({ message: "Ошибка при выводе одного пользователя", err: error }));
+  user
+    .findById(req.params.userId)
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      }
+      res.send({ message: "Пользователя с данным ID не существует" });
+    })
+    .catch((error) => res
+      .status(500)
+      .send({ message: "Ошибка при выводе одного пользователя", err: error }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  user.create({ name, about, avatar })
+  user
+    .create({ name, about, avatar })
     .then((newUser) => res.send({ data: newUser }))
-    .catch((error) => res.status(500).send({ message: "Ошибка при создании пользователя", err: error }));
+    .catch((error) => res
+      .status(500)
+      .send({ message: "Ошибка при создании пользователя", err: error }));
 };
